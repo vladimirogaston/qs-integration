3f5800fd73bba4272d576782929b8e8b01746895
vladimiro <vladimirogaston@gmail.com>
2021-09-30 14:26:58 -0300

    rf help.txt && set mktstconv to creates fails testing

--- src/shared/makeTestConveyor.function.js
+++ src/shared/makeTestConveyor.function.js
@@ -1,16 +1,16 @@
 const ProductConveyorToCRM = require('../core/ProductConveyorToCRM')
 const ProductsDAO = require('../infraestructure/data/ProductsDAO.mock')
 const LogsDAO = require('../infraestructure/data/LogsDAO.mock')
-const ZohoApiClient = require('../infraestructure/rest/ZohoApiClient.mock')
-//const ZohoApiClient = require('../infraestructure/rest/ZohoApiClient')
+//const ZohoApiClient = require('../infraestructure/rest/ZohoApiClient.mock')
+const ZohoApiClient = require('../infraestructure/rest/ZohoApiClient')
 const LogsAspect = require('./ConsoleLoggerProxy')
 
 function makeTestConveyor() {
     const cantProd = 5
     let productsDao = new ProductsDAO(cantProd)
     let logsDao = new LogsDAO()
-    let client = new ZohoApiClient('Products', false, true, false)
-    //let client = new ZohoApiClient('Products')
+    //let client = new ZohoApiClient('Products', false, true, false)
+    let client = new ZohoApiClient('Products')
     let conveyor = new ProductConveyorToCRM(productsDao, client, logsDao)
 
     const logs = new LogsAspect()
